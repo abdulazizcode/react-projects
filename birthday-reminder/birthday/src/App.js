72a419ea8ac7f2f3c1b1795d89d70006cf5020db
abdulazizcode <tajikcoder@gmail.com>
2021-03-12 19:31:56 +0800

    [COM]-project useState compleated

--- birthday-reminder/birthday/src/App.js
+++ birthday-reminder/birthday/src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import React, {useState} from 'react';
+import data from "./data";
+import List from "./List";
+
 
 function App() {
+  const [people, setPeople] = useState(data);
+
   return (
-    <div className="App">
-    
-    </div>
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button className="btn" onClick={() => setPeople([])} >Clear All</button>
+      </section>
+    </main>
   );
 }
 
