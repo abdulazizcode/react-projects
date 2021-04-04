f77ada77891c372b97557247d0812c7309551944
abdulazizcode <tajikcoder@gmail.com>
2021-04-04 18:14:07 +0800

    [ADD]- useContext project completed

--- allone/src/index.js
+++ allone/src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import "./sidebar-modal/index.css";
+import {AppProvider} from "./sidebar-modal/context";
 
 ReactDOM.render(
-    <App />,
+	<AppProvider>
+		<App />
+	</AppProvider>,
   document.getElementById('root')
 );
 
--- allone/src/sidebar-modal/Home.js
+++ allone/src/sidebar-modal/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { FaBars } from 'react-icons/fa';
+import {AppContext,useGlobalContext} from "./context";
 const Home = () => {
+	const {openSidebar, openModal} = useGlobalContext();
 	return(
 		<main>
-			<button className="sidebar-toggle">
+			<button className="sidebar-toggle" onClick={openSidebar}>
 				<FaBars/>
 			</button>
-			<button className="btn">
+			<button onClick={openModal} className="btn">
 				Show Modal
 			</button>
 		</main>
